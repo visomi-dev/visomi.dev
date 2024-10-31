7d01813808059db35ed112003cc9b21c1ecdb4eb
Michael Villalba Sotelo <visomi.dev@gmail.com>
2024-10-31 13:24:51 -0600

    feat: add contact and quote components to resume section

--- src/app/resume/me/me.component.ts
+++ src/app/resume/me/me.component.ts
@@ -1,11 +1,12 @@
 import { Component, HostBinding } from '@angular/core';
 
 import { PersonalInfoComponent } from './personal-info/personal-info.component';
+import { QuoteComponent } from './quote/quote.component';
 
 @Component({
   selector: 'app-me',
   standalone: true,
-  imports: [PersonalInfoComponent],
+  imports: [PersonalInfoComponent, QuoteComponent],
   templateUrl: './me.component.html',
   styleUrl: './me.component.css',
 })
