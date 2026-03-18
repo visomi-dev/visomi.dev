c2d6edcaeaa367f30462cb00311ac59e1066d4fd
Michael Villalba Sotelo <visomi.dev@gmail.com>
2026-03-17 20:46:47 -0600

    refactor(website): extract nav-aware min-height sizing into generic css utility

--- apps/ng-website/src/app/pages/contact/hero/hero.ts
+++ apps/ng-website/src/app/pages/contact/hero/hero.ts
@@ -7,7 +7,7 @@ import { Component } from '@angular/core';
   styleUrl: './hero.css',
 
   host: {
-    class: /* tw */ 'flex h-full flex-col justify-between -mt-20 space-y-12 md:col-span-4',
+    class: /* tw */ 'flex h-full flex-col justify-between mt-20 space-y-12 md:col-span-4',
   },
 })
 export class Hero {}
